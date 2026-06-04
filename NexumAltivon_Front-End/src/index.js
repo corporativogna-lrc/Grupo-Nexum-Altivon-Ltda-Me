2bcf7c893351353fd84d084f68a16cddb7dfc0c7
corporativogna-lrc <corporativo.gna@gmail.com>
2026-06-03 22:51:22 -0300

    corrige-acesso-direto-rotas-pages

--- NexumAltivon_Front-End/src/index.js
+++ NexumAltivon_Front-End/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
 
+const redirectPath = window.location.search;
+
+if (redirectPath.startsWith('?/')) {
+  const restoredPath = redirectPath
+    .slice(1)
+    .replace(/~and~/g, '&');
+  window.history.replaceState(null, '', restoredPath + window.location.hash);
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
