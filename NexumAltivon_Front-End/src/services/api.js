bf2126ac3d45d93a73e5a16ec1b754488ac9f596
corporativogna-lrc <corporativo.gna@gmail.com>
2026-06-05 00:43:57 -0300

    ativa ponte temporaria da api

--- NexumAltivon_Front-End/src/services/api.js
+++ NexumAltivon_Front-End/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { HTTP_UNAUTHORIZED, STORAGE_KEYS } from '../constants';
 
-const PUBLIC_API_URL = 'https://api.nexumaltivon.com';
+const PUBLIC_API_URL = 'https://herald-syndication-convicted-salad.trycloudflare.com';
 
 const getDefaultApiUrl = () => {
   if (typeof window === 'undefined') return 'http://localhost:5000';
@@ -164,3 +164,4 @@ export const integracoesAPI = {
 };
 
 export default api;
+
