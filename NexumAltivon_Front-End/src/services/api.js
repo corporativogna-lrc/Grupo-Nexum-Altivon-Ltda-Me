98013b89aa05a122a41cb6b6c1eef599577f7314
corporativogna-lrc <corporativo.gna@gmail.com>
2026-06-11 04:42:09 -0300

    estabiliza api local na porta 5011

--- NexumAltivon_Front-End/src/services/api.js
+++ NexumAltivon_Front-End/src/services/api.js
@@ -13,7 +13,7 @@ const getDefaultApiUrl = () => {
   const { hostname } = window.location;
   const isLocalhost = hostname === 'localhost' || hostname === '127.0.0.1' || hostname === '';
 
-  return isLocalhost ? 'http://localhost:5010' : PUBLIC_API_URL;
+  return isLocalhost ? 'http://localhost:5011' : PUBLIC_API_URL;
 };
 
 export const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || getDefaultApiUrl();
